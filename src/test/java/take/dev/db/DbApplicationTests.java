b27f7408f791eca602d469083ef6514256cc795e
Takee Dev <peerapolkoeram@gmail.com>
2026-05-04 21:19:33 +0700

    add init project

---
+++ src/test/java/take/dev/db/DbApplicationTests.java
@@ -0,0 +1,13 @@
+package take.dev.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
